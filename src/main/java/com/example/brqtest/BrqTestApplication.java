f8c91e19b8c61b56a1d2feaa42b052bdb4a44200
Luan Feo <luanfeo@gmail.com>
2024-01-04 14:40:19 -0300

    base of the project created with spring initializr

---
+++ src/main/java/com/example/brqtest/BrqTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.brqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrqTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrqTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brqtest/BrqTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrqTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
